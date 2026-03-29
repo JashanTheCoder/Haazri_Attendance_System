2b7bd4f3b102803aae228a820a7795ecfbca9773
khushleen <sekhonkhushleen@gmail.com>
2026-03-29 17:56:35 +0530

    upload

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require("./User");
+const Course = require("./Course");
+const Attendance = require("./Attendance");
+const Enrollment = require("./Enrollment");
+
+module.exports = {
+  User,
+  Course,
+  Attendance,
+  Enrollment
+};
