3de30f9335c4f2b0b24dde9fea62876a47968f26
Jashan <jashan72464@gmail.com>
2026-03-31 23:16:05 +0530

    feat: add attendance model and attendance view with core logic integration

--- models/Attendance.js
+++ models/Attendance.js
@@ -1,47 +1,45 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../Config/db");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../Config/db');
 
-const Attendance = sequelize.define("Attendance", {
+const Attendance = sequelize.define('Attendance', {
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		primaryKey: true,
+	},
+	//for the user id we will use the foreign key to link it to the user table
+	UserId: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: 'Users',
+			key: 'id',
+		},
+	},
+	//for coursename we will use the foreign key to link it to the course table
+	courseName: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
 
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
+	delivered: {
+		type: DataTypes.INTEGER,
+		defaultValue: 0,
+	},
 
-  UserId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "Users",
-      key: "id"
-    }
-  },
+	attended: {
+		type: DataTypes.INTEGER,
+		defaultValue: 0,
+	},
 
-  courseName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+	percentage: {
+		type: DataTypes.FLOAT,
+		defaultValue: 0,
+	},
 
-  delivered: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-
-  attended: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-
-  percentage: {
-    type: DataTypes.FLOAT,
-    defaultValue: 0
-  },
-
-  date: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  }
-
+	date: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW,
+	},
 });
 
 module.exports = Attendance;
